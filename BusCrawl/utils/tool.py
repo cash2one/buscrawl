d02bac2646a582d6b9598ce27516cfed74dde4ef
xiangleilei <xiangleilei@12308.com>
2015-12-03 15:19:08 +0800

    d

--- BusCrawl/utils/tool.py
+++ BusCrawl/utils/tool.py
@@ -3,9 +3,10 @@
 from pypinyin import pinyin
 import pypinyin
 
+
 def get_pinyin_first_litter(hanzi):
     pinyin_list = pinyin(hanzi, style=pypinyin.FIRST_LETTER)
     pinyin_st = ''
     for i in pinyin_list:
-       pinyin_st+=i[0]
+        pinyin_st += i[0]
     return pinyin_st

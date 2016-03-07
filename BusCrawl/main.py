b1f1f3dbcaa7eacd311f5ac7798d2f2b8fb5471c
xiangleilei <xiangleilei@12308.com>
2016-03-07 16:14:05 +0800

    d

--- BusCrawl/main.py
+++ BusCrawl/main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import sys
 
@@ -5,4 +6,4 @@ from scrapy import cmdline
 
 path = os.path.dirname(__file__)
 sys.path.append(os.path.join(path, ".."))
-cmdline.execute("scrapy crawl kuaiba".split())
+cmdline.execute("scrapy crawl kuaiba -a province=北京".split())

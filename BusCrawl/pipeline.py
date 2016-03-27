f2aced5bb225d42856873a099450886ff75846f2
luocky <luojunping@qq.com>
2016-03-27 17:07:27 +0800

    wxsz

--- BusCrawl/pipeline.py
+++ BusCrawl/pipeline.py
@@ -22,11 +22,11 @@ class MongoPipeline(object):
             spider.logger.error(err)
             return
         if item["full_price"] < 5:
-            continue
+            return
 
         data = dict(item)
         now = dte.now()
-        if data["province"] in ["江苏", "重庆"]:        # 后续会统一用这种方式
+        if data["s_province"] in ["江苏", "重庆"]:        # 后续会统一用这种方式
             data["line_id"]= md5("%(s_city_name)s-%(d_city_name)s-%(drv_datetime)s-%(s_sta_name)s-%(d_sta_name)s-%(crawl_source)s" % data)
         else:
             data["line_id"]= md5("%(s_city_name)s-%(d_city_name)s-%(drv_datetime)s-%(bus_num)s-%(crawl_source)s" % data)

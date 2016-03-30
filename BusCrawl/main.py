48de86b65bc34f2a2714ffd673b5d1604532c8ff
xiangleilei <xiangleilei@12308.com>
2016-03-30 18:01:41 +0800

    d

--- BusCrawl/main.py
+++ BusCrawl/main.py
@@ -6,10 +6,11 @@ from scrapy import cmdline
 
 path = os.path.dirname(__file__)
 sys.path.append(os.path.join(path, ".."))
-cmdline.execute("scrapy crawl kuaiba -a province=北京".split())
+# cmdline.execute("scrapy crawl kuaiba -a province=北京".split())
 
-# cmdline.execute("scrapy crawl scqcp -a city=成都市".split())
+# cmdline.execute("scrapy crawl scqcp -a city=德阳市".split())
 # cmdline.execute("scrapy crawl bjky".split())
 
 # cmdline.execute("scrapy crawl bus100 -a province_id=450000".split())
 # cmdline.execute("scrapy crawl lnky -a province_id=210000 -a city=营口市".split())
+cmdline.execute("scrapy crawl ctrip -a province=北京".split())

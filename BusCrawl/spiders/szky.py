47678ab4fb960b5aaca867663cbb620a70baa934
xiangleilei <xiangleilei@12308.com>
2016-06-23 14:55:31 +0800

    szky

--- BusCrawl/spiders/szky.py
+++ BusCrawl/spiders/szky.py
@@ -5,13 +5,16 @@ import scrapy
 import json
 import datetime
 import requests
+import random
+import urllib
 
 from datetime import datetime as dte
 from BusCrawl.item import LineItem
 from base import SpiderBase
+from BusCrawl.middleware import BrowserRandomUserAgentMiddleware
 
 from BusCrawl.utils.tool import get_pinyin_first_litter
-from BusCrawl.utils.tool import get_redis
+from BusCrawl.utils.tool import get_redis, trans_js_str, vcode_cqky
 from scrapy.conf import settings
 from pymongo import MongoClient
 
@@ -25,83 +28,138 @@ class SzkySpider(SpiderBase):
 
         "DOWNLOADER_MIDDLEWARES": {
             'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
-            'BusCrawl.middleware.MobileRandomUserAgentMiddleware': 400,
-            'BusCrawl.middleware.ProxyMiddleware': 410,
-            'BusCrawl.middleware.SzkyHeaderMiddleware': 410,
+#             'BusCrawl.middleware.MobileRandomUserAgentMiddleware': 400,
+#             'BusCrawl.middleware.ProxyMiddleware': 410,
+#             'BusCrawl.middleware.SzkyHeaderMiddleware': 410,
         },
 #        "DOWNLOAD_DELAY": 0.1,
        "RANDOMIZE_DOWNLOAD_DELAY": True,
     }
 
-    def start_requests(self):
-        start_url = "http://www.vchepiao.cn/mb/base/bus/queryNewSKY"
-        station_dict = {
-                        "B1K003": '福田汽车客运站',
-                        "B1K002": "深圳湾客运服务点",
-                        "B1K004": "南山汽车客运站",
-                        "B1K005": "盐田汽车客运站",
-                        "B1K006": "东湖汽车客运站",
-                        "B2K037": "深圳北汽车客运站",
-                        "B1K010": "皇岗汽车客运站",
-                        "B2K040": "机场汽车客运站",
-                        }
-        for k, v in station_dict.items():
-            data = {
-                "stationCode": k,
-                }
-            yield scrapy.FormRequest(start_url,
-                                     method="POST",
-                                     formdata=data,
-                                     callback=self.parse_target_city,
-                                     meta={"start_code": k})
+    def query_cookies(self, headers):
+        cookies = {}
+        valid_code = ''
+        if not valid_code:
+            login_form = "http://124.172.118.225/UserData/UserCmd.aspx"
+            valid_url = "http://124.172.118.225/ValidateCode.aspx"
+            r = requests.get(login_form, headers=headers, cookies=cookies)
+            cookies.update(dict(r.cookies))
+            for i in range(3):
+                r = requests.get(valid_url, headers=headers, cookies=cookies)
+                if "image" not in r.headers.get('content-type'):
+                    self.modify(ip="")
+                else:
+                    break
+            cookies.update(dict(r.cookies))
+            valid_code = vcode_cqky(r.content)
 
-    def parse_target_city(self, response):
-        start_code = response.meta["start_code"]
-        res = json.loads(response.body)
-        if not res["success"]:
-            self.logger.error("parse_target_city: Unexpected return, %s", res)
-            return
-        line_url = "http://www.vchepiao.cn/mb/base/bus/queryBusSKY"
-        end_list = res['data']
-        for end in end_list:
-            today = datetime.date.today()
-            for j in range(1, 7):
-                sdate = str(today+datetime.timedelta(days=j))
-                sdate_tra = sdate.replace('-', '')
-#                 if self.has_done(start[1], end["depotName"], sdate):
-#                     self.logger.info("ignore %s ==> %s %s" % (start[1], end["depotName"], sdate))
-#                     continue
-                data = {
-                    "fromCity": "深圳",
-                    "stationCode": start_code,
-                    "dstNode": end['NDName'],
-                    "schDate": sdate_tra
+        if valid_code:
+            headers = {
+                "User-Agent": headers.get("User-Agent", ""),
+                "Referer": "http://124.172.118.225/User/Default.aspx",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "X-Requested-With": "XMLHttpRequest"
+            }
+            params = {
+                "loginID": 'a13267109876',
+                "loginPwd": '123456',
+                "getInfo": 1,
+                "loginValid": valid_code,
+                "cmd": "login",
+            }
+            login_url = "http://124.172.118.225/UserData/UserCmd.aspx"
+            r = requests.post(login_url, data=urllib.urlencode(params), headers=headers, cookies=cookies)
+            ret = json.loads(trans_js_str(r.content))
+            success = ret.get("success", True)
+            res = {}
+            if success:     # 登陆成功
+                cookies.update(dict(r.cookies))
+                if ret["F_Code"] != 'a13267109876':
+                    res.update({'status': 1})
+                    return res
+                res.update({'status': 0, 'cookies': cookies, 'valid_code':valid_code})
+                return res
+            else:
+                res.update({'status': 1})
+                return res
+
+    def start_requests(self):
+        headers = {"User-Agent": random.choice(BrowserRandomUserAgentMiddleware.user_agent_list)}
+        for i in range(50):
+            res = self.query_cookies(headers)
+            if res.get('status', '') == 0:
+                cookies = res.get('cookies')
+                valid_code = res.get('valid_code')
+                break
+        if cookies:
+            station_dict = {
+                    "B1K003": ('福田汽车站','100101'),
+                    "B1K002": ("罗湖汽车站","100301"),
+                    "B1K004": ("南山汽车站",'100702'),
+                    "B1K005": ("盐田汽车站",'100401'),
+                    "B1K006": ("东湖客运站","100501"),
+                    "B2K037": ("深圳北汽车站",'01'),
+                    "B1K010": ("皇岗客运站",'100203'),
+                    "B2K040": ("机场客运站","10005"),
                 }
-                yield scrapy.FormRequest(line_url,
-                                         method="POST",
-                                         formdata=data,
-                                         callback=self.parse_line,
-                                         meta={"start_code": start_code, "end": end, "date": sdate})
+            end_list = []
+            letter = 'abcdefghijklmnopqrstuvwxyz'
+            for i in letter:
+                for j in letter:
+                    query = i+j
+                    end_list.append(query)
+            line_url = 'http://124.172.118.225/UserData/MQCenterSale.aspx'
+            headers.update({
+                        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                        "Referer": "http://124.172.118.225/User/Default.aspx",
+                        "X-Requested-With": "XMLHttpRequest",
+                    })
+            for k, (name, w_code) in station_dict.items():
+                for end in end_list:
+                    today = datetime.date.today()
+                    for j in range(1, 3):
+                        sdate = str(today+datetime.timedelta(days=j))
+                        if self.has_done(name, end, sdate):
+                            self.logger.info("ignore %s ==> %s %s" % (name, end,sdate))
+                            continue
+                        data = {
+                            "DstNode": end,
+                            "OpAddress": "-1",
+                            "OpStation":  "-1",
+                            "OperMode": '',
+                            "SchCode": '',
+                            "SchDate": sdate,
+                            "SchTime": '',
+                            'SeatType': '',
+                            'StartStation':  k,
+                            'WaitStationCode': w_code,
+                            'cmd': "MQCenterGetClass",
+                            'txtImgCode': valid_code,
+                        }
+#                         proxies = {
+#                             'http': 'http://192.168.1.33:8888',
+#                             'https': 'http://192.168.1.33:8888',
+#                             }
+#                         r = requests.post(line_url, data=urllib.urlencode(data), headers=headers, cookies=cookies,proxies=proxies)
+#                         content = json.loads(trans_js_str(r.content))
+#                         yield self.parse_line(content)
+                        yield scrapy.FormRequest(line_url,
+                                                 method="POST",
+                                                 cookies=cookies,
+                                                 headers=headers,
+                                                 formdata=data,
+                                                 callback=self.parse_line,
+                                                 meta={"start": name, "end": end, "date": sdate}
+                                                )
 
     def parse_line(self, response):
         "解析班车"
-        start_code = response.meta["start_code"]
+        start = response.meta["start"]
         end = response.meta["end"]
         sdate = response.meta["date"]
-#         self.mark_done(start[1], end["depotName"], sdate)
-        try:
-            res = json.loads(response.body)
-        except Exception, e:
-            raise e
-#         if  res["values"]["resultList"]:
-#             print res["values"]["resultList"]
-#             print start["name"] ,end["depotName"]
-        if not res["success"]:
-            #self.logger.error("parse_line: Unexpected return, %s, %s->%s, %s", sdate, start["city_name"], end["city_name"], res["header"])
-            return
-        if res["data"]["list"]:
-            print res
-        for d in res["data"]["list"]:
+        self.mark_done(start, end, sdate)
+        res = json.loads(trans_js_str(response.body))
+        for d in res["data"]:
             if d['SchStat'] == '1':
                 attrs = dict(
                     s_province = u'广东',
@@ -112,9 +170,9 @@ class SzkySpider(SpiderBase):
                     s_sta_id = d["SchStationCode"],
                     d_city_name = d["SchDstCity"],
                     d_city_code=get_pinyin_first_litter(d["SchDstCity"]),
-                    d_city_id = d['SchStationCode'],
+                    d_city_id = d['SchDstNode'],
                     d_sta_name = d["SchNodeName"],
-                    d_sta_id = d["SchDstNode"],
+                    d_sta_id = d["SchNodeCode"],
                     drv_date = d["SchDate"],
                     drv_time = d["orderbytime"],
                     drv_datetime = dte.strptime("%s %s" % (d["SchDate"], d["orderbytime"]), "%Y-%m-%d %H:%M"),
@@ -127,7 +185,7 @@ class SzkySpider(SpiderBase):
                     fee = 0,
                     crawl_datetime = dte.now(),
                     extra_info = {"raw_info": d},
-                    left_tickets = int(d["SchSeatCount"]),
+                    left_tickets = int(d["SchTicketCount"]),
                     crawl_source = "szky",
                     shift_id="",
                 )
--- BusCrawl/utils/tool.py
+++ BusCrawl/utils/tool.py
@@ -5,6 +5,22 @@ from scrapy.conf import settings
 import pypinyin
 import hashlib
 import redis
+import re
+
+import cStringIO
+
+try:
+    import Image
+    import ImageDraw
+    import ImageFont
+    import ImageFilter
+except ImportError:
+    from PIL import Image
+    from PIL import ImageDraw
+    from PIL import ImageFont
+    from PIL import ImageFilter
+from datetime import datetime as dte
+from pytesseract import image_to_string
 
 
 def get_pinyin_first_litter(hanzi):
@@ -31,3 +47,75 @@ def get_redis(db=0):
         r = redis.StrictRedis(host=host, port=port, db=db)
         redis_list[db] = r
     return redis_list[db]
+
+
+def trans_js_str(s):
+    """
+    {aa:'bb'} ==> {"aa":"bb"}
+    """
+    for k in set(re.findall("([A-Za-z]+):", s)):
+        s = re.sub(r"\b%s\b" % k, '"%s"' % k, s)
+    s = s.replace("'", '"')
+    return s
+
+
+def vcode_cqky(img_content):
+    ims = cStringIO.StringIO(img_content)
+    im = Image.open(ims)
+    im = im.convert('L')
+    im = im.point(lambda x: 255 if x > 140 else 0)
+    im = ecp(im)
+    code = image_to_string(im, lang='kp', config='-psm 8')
+    return code
+
+
+def ecp(im):
+    frame = im.load()
+    (w, h) = im.size
+    for i in xrange(w):
+        for j in xrange(h):
+            if frame[i, j] != 255:
+                count = 0
+                try:
+                    if frame[i, j - 1] == 255:
+                        count += 1
+                except IndexError:
+                    pass
+                try:
+                    if frame[i, j + 1] == 255:
+                        count += 1
+                except IndexError:
+                    pass
+                try:
+                    if frame[i - 1, j - 1] == 255:
+                        count += 1
+                except IndexError:
+                    pass
+                try:
+                    if frame[i - 1, j] == 255:
+                        count += 1
+                except IndexError:
+                    pass
+                try:
+                    if frame[i - 1, j + 1] == 255:
+                        count += 1
+                except IndexError:
+                    pass
+                try:
+                    if frame[i + 1, j - 1] == 255:
+                        count += 1
+                except IndexError:
+                    pass
+                try:
+                    if frame[i + 1, j] == 255:
+                        count += 1
+                except IndexError:
+                    pass
+                try:
+                    if frame[i + 1, j + 1] == 255:
+                        count += 1
+                except IndexError:
+                    pass
+                if count >= 6:
+                    frame[i, j] = 255
+    return im
